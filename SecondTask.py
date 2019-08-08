6ec0eebe197d272e3224137d42ac986d012020b9
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-08-08 13:45:39 +0300

    Added new file

---
+++ SecondTask.py
@@ -0,0 +1,19 @@
+def string_conversion(s):
+    letter = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')
+    a = []
+    for i in range(len(s)):
+        if s[i] in letter:
+            a.append(s[i])
+    return ''.join(a)
+
+
+def magic_function(s):
+    s = s.split()
+    b = []
+    for i in range(len(s)):
+        b.append(''.join(list(s[i])[::-1]))
+    return ' '.join(b)
+
+
+if __name__ == '__main__':
+    print(magic_function(string_conversion('I dah ot evom tuo')))
