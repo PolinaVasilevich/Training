bbbd58e7fea256872c35d9f3d90f21c851e8e066
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-08-08 22:09:16 +0300

    Test

--- SecondTask.py
+++ SecondTask.py
@@ -2,12 +2,16 @@ import random
 
 
 def string_conversion(s):
-    letter = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')
-    a = []
-    for i in range(len(s)):
-        if s[i] in letter:
-            a.append(s[i])
-    return ''.join(a)
+    letter = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    s = s.split()
+    res = []
+    for word in s:
+        a = ''
+        for l in word:
+            if l in letter:
+                a += l
+        res.append(a)
+    return ' '.join(res)
 
 
 def magic_function(s):
@@ -19,4 +23,5 @@ def magic_function(s):
 
 
 if __name__ == '__main__':
-    print(magic_function(string_conversion('I dah ot evom tuo')))
+    print(magic_function(string_conversion('I45 dah556 ot! evom!DDDDD tuoGGG')))
+    print(string_conversion('qwef dfjdf fjdfj'))
