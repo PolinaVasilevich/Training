7737a5f4708cb14b8606f8c08b09afd3a83bc7f6
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-08-09 15:54:20 +0300

    Test

--- SecondTask.py
+++ SecondTask.py
@@ -2,13 +2,14 @@ import random
 import string
 
 s = '5ADadc1!!! s6d2,./ dv6/v3'
-
-for i, item in enumerate(s):
-    if item not in string.ascii_letters:
-        s = s.replace(item, '')
-print(s)
 s = s.split()
-
-for i, item in enumerate(s):
-    s = str(s).replace(item, item[::-1])
 print(s)
+for i, word in enumerate(s):
+    for j, letter in enumerate(word):
+        if letter not in string.ascii_letters:
+            word = word.replace(letter, '')
+
+
+#for i, item in enumerate(s):
+   # s = s.replace(item, item[::-1])
+#print(s)
