82833f140c7128fa2d1eb2245a3fe19d3cee9820
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-08-13 13:49:56 +0300

    new file

--- SecondTask.py
+++ SecondTask.py
@@ -4,12 +4,12 @@ import string
 s = '5ADadc1!!! s6d2,./ dv6/v3'
 s = s.split()
 print(s)
-for i, word in enumerate(s):
+for i, word in enumerate(s, 0):
     for j, letter in enumerate(word):
         if letter not in string.ascii_letters:
-            word = word.replace(letter, '')
-
-
+            #word = word.replace(letter, '')
+    s = str(s).replace(word, '')
+print(s)
 #for i, item in enumerate(s):
    # s = s.replace(item, item[::-1])
 #print(s)
