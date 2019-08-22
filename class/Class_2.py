92c7854270e4ee5ca82919993e83f938f784d1d5
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-08-22 08:03:26 +0300

    Added new class Vector

--- class/Class_2.py
+++ class/Class_2.py
@@ -1,17 +1,38 @@
-class Car():
-    def exclaim(self):
-        print("I'm Car!")
+"""координаты вектора — это его проекции на соответствующие координатные оси"""
 
 
-class Yugo(Car):
-    def exclaim(self):
-        print("I'm Yugo!")
+class Vector:
 
-    def need_a_push(self):
-        print("A little help here?")
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    @property
+    def pr_x(self):
+        return self.x
+
+    @property
+    def pr_y(self):
+        return self.y
+
+    @property
+    def pr_z(self):
+        return self.z
+
+    @pr_x.setter
+    def pr_x(self, x):
+        self.x = x
+
+    @pr_y.setter
+    def pr_y(self, y):
+        self.y = y
+
+    @pr_z.setter
+    def pr_z(self, z):
+        self.z = z
 
 
-give_me_a_car = Car()
-give_me_a_yugo = Yugo()
-
-print(give_me_a_yugo.need_a_push())
+if __name__ == "__main__":
+    v = Vector(1, 2, 3)
+    print("X-axis projection: {}\nY-axis projection: {}\nZ-axis projection: {}".format(v.pr_x, v.pr_y, v.pr_z))
