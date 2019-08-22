5d1f9ef1fe0368592301b153b99e4e89219ddc12
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-08-22 08:38:57 +0300

    Modification class Vector

--- class/Class_2.py
+++ class/Class_2.py
@@ -4,35 +4,36 @@
 class Vector:
 
     def __init__(self, x=0, y=0, z=0):
-        self.x = x
-        self.y = y
-        self.z = z
+        self.__x = x
+        self.__y = y
+        self.__z = z
 
     @property
     def pr_x(self):
-        return self.x
-
-    @property
-    def pr_y(self):
-        return self.y
-
-    @property
-    def pr_z(self):
-        return self.z
+        return self.__x
 
     @pr_x.setter
     def pr_x(self, x):
-        self.x = x
+        self.__x = x
+
+    @property
+    def pr_y(self):
+        return self.__y
 
     @pr_y.setter
     def pr_y(self, y):
-        self.y = y
+        self.__y = y
+
+    @property
+    def pr_z(self):
+        return self.__z
 
     @pr_z.setter
     def pr_z(self, z):
-        self.z = z
+        self.__z = z
 
 
 if __name__ == "__main__":
     v = Vector(1, 2, 3)
     print("X-axis projection: {}\nY-axis projection: {}\nZ-axis projection: {}".format(v.pr_x, v.pr_y, v.pr_z))
+
