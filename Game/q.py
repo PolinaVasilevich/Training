2c5f98493960a360e1f6e1671afd2dd4a0d31bc1
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-10-04 16:02:09 +0300

    a

--- Game/q.py
+++ Game/q.py
@@ -5,18 +5,32 @@ while True:
     n = input('First gamer: ')
     if n in s:
         indexN = s.index(n)
-        print(indexN)
         s[indexN] = '0'
         print('|'.join(s))
         if (s[1] == '0' and s[2] == '0' and s[3] == '0') or (s[1] == '0' and s[5] == '0' and s[9] == '0') or \
                 (s[9] == '0' and s[10] == '0' and s[11] == '0') or (s[11] == '0' and s[7] == '0' and s[3] == '0') or \
                 (s[1] == '0' and s[6] == '0' and s[11] == '0') or (s[3] == '0' and s[6] == '0' and s[9] == '0'):
-            print('You win!')
+            print('First gamer win!')
             answer = input('Do you want to play? Y&N: ')
             if answer == 'N':
                 break
     else:
         print('Ячейка уже занята!\nПопробуйте ещё раз.')
+        n = input('First gamer: ')
 
-
+    m = input('Second gamer: ')
+    if m in s:
+        indexN = s.index(m)
+        s[indexN] = 'X'
+        print('|'.join(s))
+        if (s[1] == 'X' and s[2] == 'X' and s[3] == 'X') or (s[1] == 'X' and s[5] == 'X' and s[9] == 'X') or \
+                (s[9] == 'X' and s[10] == 'X' and s[11] == 'X') or (s[11] == 'X' and s[7] == 'X' and s[3] == 'X') or \
+                (s[1] == 'X' and s[6] == 'X' and s[11] == 'X') or (s[3] == 'X' and s[6] == 'X' and s[9] == 'X'):
+            print('Second gamer win!')
+            answer = input('Do you want to play? Y&N: ')
+            if answer == 'N':
+                break
+    else:
+        print('Ячейка уже занята!\nПопробуйте ещё раз.')
+        m = input('Second gamer: ')
 
