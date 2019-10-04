060686c2e62af79044b43a61e5b3696155cb6132
Polina Vasilevich <polja.vasilevich@gmail.com>
2019-10-04 15:45:14 +0300

    Added new file

--- Game/q.py
+++ Game/q.py
@@ -2,17 +2,17 @@ string = '|1|2|3|\n|4|5|6|\n|7|8|9|'
 s = string.split('|')
 print(string)
 while True:
-    n = input('First: ')
-    indexN = s.index(n)
-    s[indexN] = '0'
-    print('|'.join(s))
-    if s.count('0') == 3:
-        print('You win!')
-        break
-    m = input('Second: ')
-    indexM = s.index(m)
-    s[indexM] = 'X'
-    print('|'.join(s))
-    if s.count('X') == 3:
-        print('You win!')
-        break
+    n = input('First gamer: ')
+    if n in s:
+        indexN = s.index(n)
+        print(indexN)
+        s[indexN] = '0'
+        print('|'.join(s))
+        if s[1] == '0' and s[2] == '0' and s[3] == '0':
+            print('You win!')
+            break
+    else:
+        print('Ящейка уже занята!')
+
+
+
